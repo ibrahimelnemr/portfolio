122d93460118d7ae347c9b07b0d1e8311bf7207e
Ibrahim El Nemr <70034333+iwelnemr@users.noreply.github.com>
2022-12-27 22:21:47 +0200

    add profile image and styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import logo from './logo.svg';
+import profileImg from './profile.jpg'
 import './App.css';
 import Greet from './components/Greet'
+import ProfileImage from './components/ProfileImage'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
          <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Hello World!</p>
         <p>Hello World!</p>
-      </header>
-
+      
+      <ProfileImage/>
       <Greet/>
+      </header>
     </div>
+    
   );
 }
 
---
+++ src/components/ProfileImage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import profileImg from '../profile.jpg'
+
+function ProfileImage () {
+    return <img src={profileImg} className="profile-image" alt="logo" />
+}
+
+export default ProfileImage
