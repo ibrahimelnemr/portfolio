18b66af1781f43814e178118b9fb46ed134b3ab5
Ibrahim El Nemr <70034333+iwelnemr@users.noreply.github.com>
2023-01-08 18:34:35 +0200

    install packages

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Button } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 // function Greet () {
 //     return <h1>Greet Component</h1>
 // }
 
-const Greet = () => <h1>Hello!</h1>
-export default Greet
+// const Greet = () => <h1>Hello!</h1>
+// export default Greet
+
+export default(props) => {
+    return <Button color="danger">Danger</Button>;
+}
