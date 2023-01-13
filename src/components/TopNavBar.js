61191a60a69805914b235d9dad5146ef8d30ea0a
Ibrahim El Nemr <70034333+iwelnemr@users.noreply.github.com>
2023-01-13 20:33:45 +0200

    fix issues with navbar

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -7,17 +7,17 @@ import Container from 'react-bootstrap/Container';
 function TopNavBar() {
     return(
         <>
-        <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
+        <Container className="w-100">
+        <Navbar className ="column justify-content-end">
+        <Nav className="">
             <Nav.Link href="#home">Home</Nav.Link>
+            
             <Nav.Link href="#features">Features</Nav.Link>
+            
             <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
+      </Nav>
       </Navbar>
-      <br />
+      </Container>
       
       </>
     );
