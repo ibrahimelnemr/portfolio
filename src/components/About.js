ad0220fbe9e1e88e396501f138e47da5475dfe6c
Ibrahim El Nemr <70034333+iwelnemr@users.noreply.github.com>
2023-07-01 01:27:29 +0300

    change alignment of about section

--- src/components/About.js
+++ src/components/About.js
@@ -10,8 +10,10 @@ return(
             About
         </div>
     </div>
-    
-    <div  class="col-12">
+    <div className="row p-0 m-0">
+
+
+    <div  class="col-6">
 
         <div className="d-flex justify-content-center m-5">
     
@@ -19,12 +21,14 @@ return(
         </div>
     </div>
 
-    <div class="col-12">
+    <div class="col-6">
         <div className="d-flex justify-content-center m-5 fs-4 fw-light">
             Hi! I'm Ibrahim and I am currently doing undergraduate studies in Economics with a minor in Computer Science at the American University in Cairo.
 
             I am passionate about computer science, driven to take on new projects and eager to learn. Currently looking for an opportunity in the computer science industry to grow my knowledge of emerging frameworks and technologies.
         </div>
+    </div>
+
     </div>
 </div>
 </>
