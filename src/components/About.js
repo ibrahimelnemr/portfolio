818d691676fada75ebe6c57213bbcc56ace4b6f7
Ibrahim El Nemr <70034333+iwelnemr@users.noreply.github.com>
2023-07-26 15:19:07 +0300

    improve spacing and responsiveness

--- src/components/About.js
+++ src/components/About.js
@@ -10,20 +10,22 @@ return(
             About
         </div>
     </div>
-    <div className="row p-0 m-0">
 
+<div className="row px-0 m-0">
 
-    <div  class="col-6-sm">
+    <div  class="col-sm-6 d-none d-lg-block col-lg-3">
 
         <div className="d-flex justify-content-center m-5">
-    
             <img src={profileImg} class="img-fluid" Style="border-radius: 15%;" alt="logo" />
         </div>
     </div>
 
-    <div class="col-6-sm d-flex flex-column justify-content-center">
-        <div className="m-5 fs-3 fw-light">
+    <div class="col-12 col-lg-9 fs-3 fs-lg-5 d-flex flex-column justify-content-center ">
+        <div className="m-5 fw-light">
+            <p>
             Hi! I'm Ibrahim and I am currently doing undergraduate studies in Economics with a minor in Computer Science at the American University in Cairo.
+            </p>
+            
         </div>
     </div>
 
