4737ce650a68dc3767ba1d94b642cddaf53566cc
Ibrahim El Nemr <70034333+iwelnemr@users.noreply.github.com>
2023-07-26 22:29:38 +0300

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React from 'react';
+import github_logo from '../images/github.png'
+import github_logo2 from '../images/github2.png'
+import linkedin_logo from '../images/linkedin.png'
 
 function Contact() {
 
 return(
 
 <>
-<div id="contact" class="row m-0  bg-dark text-light">
+<div id="contact" class="row m-0  bg-dark bg-opacity-75 text-light">
     <div class="col-12">
         <div class="text-center p-2 m-2 display-4">
             Get in Touch
         </div>
     </div>
-
-    
+    <div class="col-12">
+        <div class="p-2 m-2 lead fs-3">
+            
+        </div>
+    </div>
+    <div class="col-6 text-center">
+        <img class="img-fluid logo" src={linkedin_logo} alt="" />
+    </div>
+    <div class="col-6 text-center">
+        <img class="img-fluid logo" src={github_logo2} alt="" />
+    </div>
 
     
 </div>
