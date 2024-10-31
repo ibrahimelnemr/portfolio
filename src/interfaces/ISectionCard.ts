7300a24783615ef0bc7fe921c8f8019a66e15b6f
Ibrahim El Nemr <70034333+ibrahimelnemr@users.noreply.github.com>
2024-11-01 00:20:59 +0200

    Create ISectionCard.ts

---
+++ src/interfaces/ISectionCard.ts
@@ -0,0 +1,10 @@
+interface ISectionCard {
+    title: string;
+    bulletPoints?: string[];
+    text?: string;
+    link?: string;
+    linkText?: string;
+    isGithubLink?: boolean;
+}
+
+export default ISectionCard;
