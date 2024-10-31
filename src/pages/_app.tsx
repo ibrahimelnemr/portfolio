96575c560349cc25a8b56b1b74657b9d82aaf510
Ibrahim El Nemr <70034333+ibrahimelnemr@users.noreply.github.com>
2024-11-01 00:09:47 +0200

    reorganize styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/theme.scss';
 import '../styles/globals.scss';
-import '../styles/index.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
