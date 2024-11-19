7f41268aa3cb13cee7e3bbe080c369673a4c06f5
Ibrahim El Nemr <70034333+ibrahimelnemr@users.noreply.github.com>
2024-11-19 17:19:50 +0200

    fix project and certification cards

--- src/components/certificationsSection/certificationCard/index.tsx
+++ src/components/certificationsSection/certificationCard/index.tsx
@@ -1,4 +1,3 @@
-// components/certificationCard/CertificationCard.tsx
 import React from "react";
 import ISectionCard from "../../../interfaces/ISectionCard";
 
@@ -11,21 +10,25 @@ const CertificationCard: React.FC<ISectionCard> = ({
 }) => {
   return (
     <div className="lg:w-1/2 md:w-1/2 sm:w-full p-4">
-      <div className="bg-gray-800 shadow-lg rounded-lg p-5 mb-5">
-        <h3 className="text-2xl font-extralight mb-3">{title}</h3>
-        <ul className="list-disc pl-5">
-          {bulletPoints.map((bulletPoint, index) => (
-            <li key={index}>{bulletPoint}</li>
-          ))}
-        </ul>
-        <a
-          href={link}
-          target="_blank"
-          rel="noopener noreferrer"
-          className={`mt-4 inline-block py-2 px-4 rounded-md bg-gray-500 text-white hover:bg-gray-700`}
-        >
-          {linkText}
-        </a>
+      <div className="bg-gray-800 shadow-lg rounded-lg p-5 mb-5 h-full flex flex-col justify-between">
+        <div>
+          <h3 className="text-2xl font-extralight mb-3">{title}</h3>
+          <ul className="list-disc pl-5 text-gray-300">
+            {bulletPoints.map((bulletPoint, index) => (
+              <li key={index} className="line-clamp-1">{bulletPoint}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="mt-auto">
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block py-2 px-4 rounded-md bg-gray-500 text-white hover:bg-gray-700"
+          >
+            {linkText}
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/components/projectsSection/projectCard/index.tsx
+++ src/components/projectsSection/projectCard/index.tsx
@@ -1,4 +1,3 @@
-// components/projectCard/ProjectCard.tsx
 import React from "react";
 import ISectionCard from "../../../interfaces/ISectionCard";
 
@@ -11,18 +10,22 @@ const ProjectCard: React.FC<ISectionCard> = ({
 }) => {
   return (
     <div className="lg:w-1/2 md:w-1/2 sm:w-full p-4">
-      <div className="bg-gray-800 shadow-lg rounded-lg p-5 mb-5">
-        <h3 className="text-2xl font-extralight mb-3">{title}</h3>
-        <p className="mb-3">{text}</p>
-        <a
-          href={link}
-          target="_blank"
-          rel="noopener noreferrer"
-          className={`inline-block py-2 px-4 rounded-md bg-gray-500 text-white hover:bg-gray-700`}
-        >
-          <i className={`fab fa-github ${isGithubLink ? "mr-2" : ""}`}></i>
-          {linkText}
-        </a>
+      <div className="bg-gray-800 shadow-lg rounded-lg p-5 mb-5 h-full flex flex-col justify-between">
+        <div>
+          <h3 className="text-2xl font-extralight mb-3">{title}</h3>
+          <p className="mb-3 text-gray-300 line-clamp-3">{text}</p> {/* Truncate text */}
+        </div>
+        <div className="mt-auto">
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block py-2 px-4 rounded-md bg-gray-500 text-white hover:bg-gray-700"
+          >
+            <i className={`fab fa-github ${isGithubLink ? "mr-2" : ""}`}></i>
+            {linkText}
+          </a>
+        </div>
       </div>
     </div>
   );
